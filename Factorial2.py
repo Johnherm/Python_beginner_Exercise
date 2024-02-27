ba883100869ee2c302955f8ddeee4ecb9f319f1a
Yohannes <johnherma72b@gmail.com>
2024-02-27 06:18:51 -0800

    Updated factorial

--- Factorial2.py
+++ Factorial2.py
@@ -1,7 +1,7 @@
-number = int(input("Enter a number: \n "))
+num = int(input("Enter a number: \n "))
 fact = 1
 j = 1
 for i in range(1,number+1):
     j = i+1
     fact=fact*j
-print(f"{number}! = ",fact)
+print(f"{num}! = ",fact)
