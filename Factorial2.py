9189cf303bc86cfb3534a0d1712ce553840299ff
Yohannes <johnherma72b@gmail.com>
2024-02-27 07:09:12 -0800

    Second commit

--- Factorial2.py
+++ Factorial2.py
@@ -1,7 +1,11 @@
 num = int(input("Enter a number: \n "))
 fact = 1
 j = 1
-for i in range(1,number+1):
+for i in range(1,num+1):
     j = i+1
     fact=fact*j
 print(f"{num}! = ",fact)
+print(fact)
+fact2 = fact
+list_fact2 = list(fact2)
+print(list_fact2)
---
+++ PasswordCrack.py
@@ -0,0 +1,11 @@
+import random, pyautogui 
+char = "1234567890abcdefghijklmnopqrstutvwxz"
+char_list = list(char)
+password = pyautogui.password("Enter your password")
+guess_password = ''
+while (guess_password!=password):
+    guess_password = random.choices(char_list, k= len(password))
+    print("{{{{{}}}}}"+str(guess_password)+"{{{{{{}}}}}}")
+    if (guess_password == list(password)):
+        print("Your Cracked Password is : "+"".join(guess_password))
+        break
